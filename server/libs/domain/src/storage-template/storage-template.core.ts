28d7bf5eda008bf2f771fb8956d44d326f3dd344
Jason Rasmussen <jrasm91@gmail.com>
2023-05-09 17:32:05 -0400

    fix: sanitize username for use in folder path

--- server/libs/domain/src/storage-template/storage-template.core.ts
+++ server/libs/domain/src/storage-template/storage-template.core.ts
@@ -41,7 +41,7 @@ export class StorageTemplateCore {
       const source = asset.originalPath;
       const ext = path.extname(source).split('.').pop() as string;
       const sanitized = sanitize(path.basename(filename, `.${ext}`));
-      const rootPath = this.storageCore.getFolderLocation(StorageFolder.LIBRARY, username || asset.ownerId);
+      const rootPath = this.storageCore.getFolderLocation(StorageFolder.LIBRARY, sanitize(username || asset.ownerId));
       const storagePath = this.render(this.storageTemplate, asset, sanitized, ext);
       const fullPath = path.normalize(path.join(rootPath, storagePath));
       let destination = `${fullPath}.${ext}`;
