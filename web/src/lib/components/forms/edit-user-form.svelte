805508788085fdf59b86a3b810f5e1976b104139
Jason Rasmussen <jrasm91@gmail.com>
2023-05-10 07:04:41 -0400

    Update web/src/lib/components/forms/edit-user-form.svelte
    Co-authored-by: Michel Heusschen <59014050+michelheusschen@users.noreply.github.com>

--- web/src/lib/components/forms/edit-user-form.svelte
+++ web/src/lib/components/forms/edit-user-form.svelte
@@ -82,7 +82,7 @@
 		</div>
 
 		<div class="m-4 flex flex-col gap-2">
-			<label class="immich-form-label" for="lastName">Username</label>
+			<label class="immich-form-label" for="username">Username</label>
 			<input
 				class="immich-form-input"
 				id="username"
